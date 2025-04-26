718c6f41e17d693d276c0fd6c080911255a587ed
samyfg41 <samanthafaruolo@gmail.com>
2025-04-26 01:39:51 -0300

    Mudança de código, adição de imagens, adição de css e adição de javascript

---
+++ javascript/script.js
@@ -0,0 +1,9 @@
+function atualizaContador() {
+    const destino = new Date("2025-11-12T00:00:00").getTime(); // nova data
+    const agora = new Date().getTime();
+    const distancia = destino - agora;
+
+    const dias = Math.floor(distancia / (1000 * 60 * 60 * 24));
+    document.getElementById("contador").textContent = `Faltam ${dias} dias para a viagem! 🏰`;
+}
+window.onload = atualizaContador;
